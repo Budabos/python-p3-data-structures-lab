120eaaacd95229e8db8230c0ee940aeafd1c2d75
caroline opiyo <opiyocaroline20@gmail.com>
2023-11-22 19:56:19 +0300

    python-p3-data-structures-lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,54 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [food["name"] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        heat_level = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+    return None
+    
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spiciest_foods)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total_heat = sum(food["heat_level"] for food in spicy_foods)
+    return total_heat // len(spicy_foods) if len(spicy_foods) > 0 else 0
+
+# data_structures.py
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    """
+    Add a new spicy food to the list of spicy_foods.
+
+    Parameters:
+    - spicy_foods (list): The list of existing spicy foods.
+    - spicy_food (dict): The dictionary representing the new spicy food.
+
+    Returns:
+    - list: The updated list of spicy_foods.
+    """
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
+# Example usage:
+new_spicy_food = {
+    "name": "Spicy Ramen",
+    "cuisine": "Japanese",
+    "heat_level": 8,
+}
+
+spicy_foods = create_spicy_food(spicy_foods, new_spicy_food)
+print(spicy_foods)
+
